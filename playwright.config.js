f711f6a61a0d09850c32769bb2a03f51f1e5946c
Stavri Gega <sg@webnorth.com>
2025-12-11 21:42:00 +0100

    added github

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],['github']],
   
 
 
